1c4c627195d788e9d34aa410b3745a160840ac26
jervis1994 <hjm1994jervis@163.com>
2020-08-12 11:02:04 +0800

    '初始化项目‘

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+<<<<<<< HEAD
+  <div id="app">
+=======
+  <div id="app" class="wrapper">
+>>>>>>> origin/master
+    <router-view/>
+    <main-tab-bar/>
+  </div>
+</template>
+
+<script>
+  import MainTabBar from 'components/content/mainTabbar/MainTabBar'
+
+  export default {
+    name: 'app',
+    components: {
+      MainTabBar
+    }
+  }
+</script>
+
+<style>
+  @import "assets/css/base.css";
+</style>
